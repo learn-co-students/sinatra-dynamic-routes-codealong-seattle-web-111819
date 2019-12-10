f5f94c596e23d053ed264203c485a75945f94a4c
Alice Won <alicewon4@gmail.com>
2019-12-10 11:08:55 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,17 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # This is a sample static route.
   get '/hello' do
     "Hello World!"
   end
 
-  # This is a sample dynamic route.
+
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
 
-end
+    get "/goodbye/:name" do
+      @user_name = params[:name]
+      "Goodbye, #{@user_name}."
+    end
+
+    #A dynamic route starting with /multiply that accepts two params (num1 and num2) and returns the product of the two numbers.
+    get "/multiply/:num1/:num2" do
+      "#{params[:num1].to_i * params[:num2].to_i}"
+    end
+  end
